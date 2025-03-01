5491484e14d5d5f421d23e65165b20c1a9a38eb9
Emerson Roncancio <roncancioavila@gmail.com>
2025-03-01 02:53:55 -0500

    Mapeo de datos con @for y uso del router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { CountComponent } from './pages/count/count.component';
 import { HeroComponent } from './pages/hero/hero.component';
+import { DragonballComponent } from './pages/dragonball/dragonball.component';
 
 export const routes: Routes = [
   {
@@ -10,5 +11,13 @@ export const routes: Routes = [
   {
     path: 'hero',
     component: HeroComponent,
+  },
+  {
+    path: 'dragonball',
+    component: DragonballComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '',
   }
 ];
