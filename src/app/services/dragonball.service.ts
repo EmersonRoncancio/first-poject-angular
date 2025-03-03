0b4a473662ab9fdd374e05c0a86dc77ce354baa2
Emerson Roncancio <roncancioavila@gmail.com>
2025-03-03 16:19:28 -0500

    Manejo de efectos y localStorage

--- src/app/services/dragonball.service.ts
+++ src/app/services/dragonball.service.ts
@@ -1,14 +1,22 @@
-import { Injectable, signal } from '@angular/core';
+import { effect, Injectable, signal } from '@angular/core';
 import { Character } from '../interfaces/character.interface';
+
+const getLocalSorage = (): Character[] => {
+  const characters = localStorage.getItem('characters')
+  if(characters){
+    return JSON.parse(characters)
+  }
+  return []
+}
 
 @Injectable({providedIn: 'root'})
 export class DragonballService {
 
-  character = signal<Character[]>([
-    {name: 'Goku', power: 'Super Saiyan'},
-    {name: 'Vegeta', power: 'Super Saiyan'},
-    {name: 'Gohan', power: 'Super Saiyan'},
-  ])
+  character = signal<Character[]>(getLocalSorage())
+
+  saveToLocalStorage = effect(()=>{
+    localStorage.setItem('characters', JSON.stringify(this.character()))
+  })
 
   addCharacter(character: Character){
     this.character.update(char=> [...char, character])
